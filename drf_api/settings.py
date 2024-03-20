7fa346e581c265f831963ef04511e15578bd1410
annieross10 <annieross10@outlookcom>
2024-03-20 14:11:13 +1000

    update permissions

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -138,7 +138,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     } if 'DEV' in os.environ else dj_database_url.parse(
-        os.environ.get('DATABASE_URL', print('connected'))
+        os.environ.get('DATABASE_URL')
     ))
 }
 
