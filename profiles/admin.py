dc275720d66e1a331a0b361123cd987d53e7e3d6
annieross10 <annieross10@outlookcom>
2024-03-20 14:40:49 +1000

    add post admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import Profile
 
 admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'owner', 'bio', 'birth_date', 'profile_picture']
