e5607f6fba84b40c8abc88b17d77b8c7404eccb7
annieross10 <annieross10@outlookcom>
2024-03-20 13:25:13 +1000

    fix bug in settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -106,7 +106,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
@@ -139,7 +138,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     } if 'DEV' in os.environ else dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
+        os.environ.get('DATABASE_URL', print('connected'))
     ))
 }
 
