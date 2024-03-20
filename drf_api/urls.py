6ad554c981d4f73b942bb12ecd9adbfadb1a6c82
annieross10 <annieross10@outlookcom>
2024-03-20 10:37:12 +1000

    dj-rest-auth Bug Fix

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,11 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from .views import root_route, logout_route
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('profiles.urls')),
@@ -13,5 +15,4 @@ urlpatterns = [
     path('', include('savedpost.urls')),
     path('', include('likes.urls')),
     path('', include('friends.urls')),
-    path('', root_route)
 ]
