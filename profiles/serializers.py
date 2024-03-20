2c25e49e2e68fb30cb36ef266784b3f940f4e849
annieross10 <annieross10@outlookcom>
2024-03-20 15:53:58 +1000

    fix profile views error

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,8 @@
+# serializers.py
+
 from rest_framework import serializers
 from .models import Profile
-from friends.models import Friend
+from friends.models import Friend  
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -8,17 +10,16 @@ class ProfileSerializer(serializers.ModelSerializer):
     friend_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
+        request = self.context.get('request')
+        return request.user == obj.owner if request else False
 
     def get_friend_id(self, obj):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            friend = Friend.objects.filter(sender=user, receiver=obj.owner).first()
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            friend = Friend.objects.filter(sender=request.user, receiver=obj.owner).first()
             return friend.id if friend else None
         return None
 
-
     class Meta:
         model = Profile
-        fields = ['id', 'owner', 'bio', 'birth_date', 'profile_picture', 'is_owner', 'friend_id',]
+        fields = ['id', 'owner', 'bio', 'birth_date', 'profile_picture', 'is_owner', 'friend_id']
