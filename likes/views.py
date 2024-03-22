9c11213d5795e317ac2f338d61f4385feeb564d8
annieross10 <annieross10@outlook.com>
2024-03-22 09:20:04 +0000

    change POST request in like API View

--- likes/views.py
+++ likes/views.py
@@ -1,4 +1,4 @@
-# views.py
+
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Like, Love, Laugh
@@ -7,7 +7,9 @@ from .serializers import LikeSerializer, LoveSerializer, LaughSerializer
 class LikeList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LikeSerializer
-    queryset = Like.objects.all()
+
+    def get_queryset(self):
+        return Like.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -20,7 +22,9 @@ class LikeDetail(generics.RetrieveDestroyAPIView):
 class LoveList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LoveSerializer
-    queryset = Love.objects.all()
+
+    def get_queryset(self):
+        return Love.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -33,7 +37,9 @@ class LoveDetail(generics.RetrieveDestroyAPIView):
 class LaughList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LaughSerializer
-    queryset = Laugh.objects.all()
+
+    def get_queryset(self):
+        return Laugh.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
