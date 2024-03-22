f1f113c31b3f3d55a0bef209a4f100f3fcfd8d43
annieross10 <annieross10@outlook.com>
2024-03-22 12:31:07 +0000

    update admin likes

--- likes/admin.py
+++ likes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Like, Laugh, Love
 
 # Register your models here.
+admin.site.register(Like)
+admin.site.register(Laugh)
+admin.site.register(Love)
