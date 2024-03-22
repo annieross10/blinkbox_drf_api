94751730568b566277050bcf25736181215dc7ab
annieross10 <annieross10@outlook.com>
2024-03-22 09:33:58 +0000

    add createAPIView to like, love, laugh

--- likes/urls.py
+++ likes/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from likes import views
 
 urlpatterns = [
-    path('likes/', views.LikeList.as_view()),
-    path('likes/<int:pk>/', views.LikeDetail.as_view()),
-    path('loves/', views.LoveList.as_view()),
-    path('loves/<int:pk>/', views.LoveDetail.as_view()),
-    path('laughs/', views.LaughList.as_view()),
-    path('laughs/<int:pk>/', views.LaughDetail.as_view()),
+    path('likes/', views.LikeCreateAPIView.as_view()),
+    path('loves/', views.LoveCreateAPIView.as_view()),
+    path('laughs/', views.LaughCreateAPIView.as_view()),
 ]
--- likes/views.py
+++ likes/views.py
@@ -1,50 +1,31 @@
 
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Like, Love, Laugh
 from .serializers import LikeSerializer, LoveSerializer, LaughSerializer
 
-class LikeList(generics.ListCreateAPIView):
+class LikeCreateAPIView(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    serializer_class = LikeSerializer
-
-    def get_queryset(self):
-        return Like.objects.all()
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-class LikeDetail(generics.RetrieveDestroyAPIView):
-    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
 
-class LoveList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    serializer_class = LoveSerializer
-
-    def get_queryset(self):
-        return Love.objects.all()
-
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class LoveDetail(generics.RetrieveDestroyAPIView):
-    permission_classes = [IsOwnerOrReadOnly]
+class LoveCreateAPIView(generics.CreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LoveSerializer
     queryset = Love.objects.all()
 
-class LaughList(generics.ListCreateAPIView):
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LaughCreateAPIView(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LaughSerializer
-
-    def get_queryset(self):
-        return Laugh.objects.all()
+    queryset = Laugh.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class LaughDetail(generics.RetrieveDestroyAPIView):
-    permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = LaughSerializer
-    queryset = Laugh.objects.all()
